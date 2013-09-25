eb7a37e6a62d0beeac64b04a15defb9aa078617a
eldermyoshida <elder.yoshida@gmail.com>
2013-09-24 21:42:21 -0400

    xcode files

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Devil-Shell
+//
+//  Created by Yoshi on 9/24/13.
+//  Copyright (c) 2013 Yoshi. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
